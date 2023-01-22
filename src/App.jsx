df05831272a8b3aefbc03eae5ca1c6367d277d41
Denisse Orellana <denissep.orellana@gmail.com>
2023-01-21 23:30:56 -0300

    add: component logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useMemo, useCallback } from 'react';  // core of react
+import Logo from './components/Logo/Logo'
 import TVShowDetail from './components/TVShowDetail/TVShowDetail'   // core of react
 
 import { TVShowAPI } from './api/tv-show';
@@ -27,7 +28,32 @@ function App() {
   return (
     <div 
       className={s.main_container}
+      style={{
+        background: currentTVShow
+        ? 
+          `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)),
+            url("${ BACKDROP_BASE_URL }${currentTVShow.backdrop_path}") no-repeat center / cover
+          `
+        : 'black',
+      }}
     >
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <Logo
+              title="WatchShows"
+              subtitle="Find a show you may like"
+              image={logoImg}
+            />
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_details}>
+        { currentTVShow && <TVShowDetail tvShow={currentTVShow} /> }
+      </div>
       <div className={s.recomended_shows}>Recomended tv shows</div>
     </div>
   )
