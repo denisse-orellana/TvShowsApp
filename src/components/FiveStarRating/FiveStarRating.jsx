4d897d6ad219d69c68966e9fce3d056fd001f1c9
Denisse Orellana <denissep.orellana@gmail.com>
2023-01-22 10:16:30 -0300

    add: rating logic

--- src/components/FiveStarRating/FiveStarRating.jsx
+++ src/components/FiveStarRating/FiveStarRating.jsx
@@ -1,23 +1,27 @@
 import { StarFill, StarHalf, Star as StarEmpty } from 'react-bootstrap-icons';
 
-const FiveStarRating = ({ rating }) => {
+const FiveStarRating = ({ rating }) => {  // 4.5 rating
 
   // Declare star icon array
   const starList = [];
 
   // Store number of filled stars
-  const starFillCount = Math.floor(rating);
+  const starFillCount = Math.floor(rating);   // 4
 
   // Store if yes or no is a half star
-  const hasHalfStar = rating - parseInt(rating) >= 0.5;
-  // ej: 4.35 - 4  
+  const hasHalfStar = rating - parseInt(rating) >= 0.5;     // ej: 4.35 - 4 = 0.35 >= 0.35 = false
 
   // Store number of emty stars
-  const emptyStarCount = 5 - starFillCount - (hasHalfStar ? 1 : 0);
+  const emptyStarCount = 5 - starFillCount - (hasHalfStar ? 1 : 0);   // ej: 5 - 4 -0 = 1
 
   // Push the filled stars icons
   for (let i = 1; i <= starFillCount; i++) {
-    starList.push(<StarFill key={"star-fill" + 1} />);
+    starList.push(<StarFill key={"star-fill" + i} />);
+  }
+
+  // Push the empty stars icons
+  for (let i = 1; i <= emptyStarCount; i++) {
+    starList.push(<StarEmpty key={'star-fill' + i} />)
   }
 
   return <div>{starList}</div>
