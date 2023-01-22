1a77ba1775777e5173a73168b2b424c00ec6eec5
Denisse Orellana <denissep.orellana@gmail.com>
2023-01-22 10:36:05 -0300

    add: new components

---
+++ src/components/TVShowList/TVShowList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TVShowList = () => {
+  return (
+    <div>TVShowList</div>
+  )
+}
+
+export default TVShowList;
---
+++ src/components/TVShowListItem/TVShowListItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TVShowListItem = () => {
+  return (
+    <div>TVShowListItem</div>
+  )
+}
+
+export default TVShowListItem;
