adfc35c5c83a83fcd931d4c94396dd1ab44c46a2
Denisse Orellana <denissep.orellana@gmail.com>
2023-02-01 19:51:10 -0300

    add:vit configurations

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 4270,
+  },
+  build: {
+    // acelerar proceso de compilacion de los archivos 
+    incremental: true,
+    // habilitar un trabajo conjunto con Babel para la interpretacion correcta de las versiones de js
+    babel: {
+      pressets: [ "@babel/present-env", "@babel/preset-react"],
+    },
+    // Caso de Typescript: habilitar la acelarion de la compilacion de TS hacia JS
+    // typescript: {
+    //   tsconfig: "./config.json";
+    // },
+    // habilitar los cambios mediante caché
+    cache: true,
+    // habiliitar la opcion de compresion para minimizar el tamaño de los archivos compilados
+    minify: true,
+    // habilitar modo debug para las ejecuciones de la generacion del build 
+    debug: true,
+  }
 })
